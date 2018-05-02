9c8f4662f3f27752066020d93a150a3d95769bac
Stefan Bols <stefan@imbols.net>
2018-05-02 14:16:29 +0200

    Small startup change

--- scripts/inject.js
+++ scripts/inject.js
@@ -7,5 +7,18 @@ try {
 if (!token) {
     alert('You have to be logged in ;-)');
 } else {
-    alert(token);
+    var textArea = document.createElement("textarea");
+    textArea.value = token;
+    document.body.appendChild(textArea);
+    textArea.focus();
+    textArea.select();
+    var successful = document.execCommand('copy');
+
+    if (successful) {
+        alert('Bearer token copied to clipboard!');
+        document.body.removeChild(textArea);
+    } else {
+        alert('Token couldn\'t be copied to clipboard.\nCheck bottom of this page for a textarea with the token.');
+    }
 }
+
--- scripts/load.js
+++ scripts/load.js
@@ -2,7 +2,7 @@
 chrome.browserAction.onClicked.addListener(function (tab) { //Fired when User Clicks ICON
     if (tab.url.toLowerCase().indexOf('xena.biz') === -1 &&
         tab.url.toLowerCase().indexOf('localhost') === -1) {
-        alert('You have to be on a Xena platform to use Toggle Culture!');
+        alert('You have to be on a Xena platform to use Get Bearer Token!');
         return;
     }
     chrome.tabs.executeScript(tab.ib, {
